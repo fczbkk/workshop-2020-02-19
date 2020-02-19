8e1fbe6e853d82aea34dbe2fae0ca75079d02830
Riki Fridrich <riki@fczbkk.com>
2020-02-19 15:29:29 +0100

    feat: add Eslint support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {get} from 'lodash-es'
-import add from './add.js'
+import {get} from 'lodash-es';
+import add from './add.js';
 
 const myObject = {
   aaa: 'bbb'
-}
+};
 
-console.log(get(myObject, 'aaa'))
+console.log(get(myObject, 'aaa'));
 
-console.log(add(1, 2))
+console.log(add(1, 2));
